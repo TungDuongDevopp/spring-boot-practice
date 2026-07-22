962b38d0235ca92dfd029c7a21d2e3215cc5f599
TungDuong-Dev <devopptungduong@gmail.com>
2026-07-22 16:46:57 +0700

    Blind css,js,html, data model in Spring

--- spring-demo/src/main/java/com/tungduong/springdemo/HelloController.java
+++ spring-demo/src/main/java/com/tungduong/springdemo/HelloController.java
@@ -1,12 +1,31 @@
 package com.tungduong.springdemo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
-@RestController  // Controller rest chuyên nhận request HTTP trả trực tiếp dữ liệu
+
+@Controller  // Controller rest chuyên nhận request HTTP trả trực tiếp dữ liệu
 public class HelloController {
+    @ModelAttribute("name")
+    public String name() {
+        return "Dương";
+    }
+
     @GetMapping("/") // Khi người dùng gọi get thì spring sẽ gọi hàm này
     public String hello() {
-        return "Hello World";
+        return "html/index";
     }
+
+    @GetMapping("/aboutus") // Khi người dùng gọi get thì spring sẽ gọi hàm này
+    public String about() {
+        return "html/about";
+    }
+    @GetMapping("/contactus") // Khi người dùng gọi get thì spring sẽ gọi hàm này
+    public String contact() {
+        return "html/contact";
+    }
+
+
 }
